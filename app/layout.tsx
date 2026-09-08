13555777c5154473f5fa64eb27ca9c23ba19c178
Grigor Safaryan <grigor.safaryan@ekeng.am>
2026-09-08 10:28:58 +0400

    feat: user scalable to 0

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,10 @@ export default function RootLayout({
   return (
     <html lang="hy" suppressHydrationWarning>
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
